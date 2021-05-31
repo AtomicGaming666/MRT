3913c634510ef493f259c6cac313c633e8ca3e46
AtomicGaming666 <39306849+AtomicGaming666@users.noreply.github.com>
2021-05-31 10:59:28 +1000

    This Project is WIP

---
+++ mordhau-rcon-tool-master/Mordhau-RCON/InfoForm.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mordhau_RCON
+{
+    public partial class InfoForm : Form
+    {
+        public InfoForm()
+        {
+            InitializeComponent();
+            lblVersion.Text = "Version: " + Application.ProductVersion;
+        }
+
+        private void CopyMail(object sender, EventArgs e)
+        {
+            Clipboard.SetText("ashflannery92@yahoo.com");
+        }
+
+        private void mailClicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("mailto:ashflannery92@yahoo.com");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText("AtomicGaming666#8864");
+        }
+
+        private void linkLabelGithub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("#");
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+    }
+}
